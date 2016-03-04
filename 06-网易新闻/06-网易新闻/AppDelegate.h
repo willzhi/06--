2ec80e72ec6ed81b4fb2d2f17587098e9c55bd0b
zhangping <czbkiosweibo@163.com>
2016-03-04 09:35:19 +0800

    创建项目

---
+++ 06-网易新闻/06-网易新闻/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  06-网易新闻
+//
+//  Created by Apple on 16/3/4.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 06-网易新闻/06-网易新闻/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  06-网易新闻
+//
+//  Created by Apple on 16/3/4.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
